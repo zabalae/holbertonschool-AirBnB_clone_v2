f38709d759caaf910224ddeec1e66a2266b9c758
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-11 13:28:23 -0400

    Task 1 hbnb_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,20 +4,22 @@ Script that starts a Flask web application
 '''
 
 
-if __name__ == "__main__":
-    from flask import Flask
+from flask import Flask
 
 
-    app = Flask(__name__)
-    app.url_map.strict_slashes = False
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-    @app.route('/')
-    def index():
-        return "Hello HBNB!"
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
 
 
-    @app.route('/hbnb')
-    def index2():
-        return "HBNB"
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

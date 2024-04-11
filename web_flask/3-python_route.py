31c4228aca846ed1b935b622d96bf989cccf02cb
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-11 13:53:35 -0400

    Task 3 python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    return "C {}".format(text).replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def isnumber(text="is cool"):
+    return "Python {}".format(text).replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

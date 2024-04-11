aac7d4e80a3c85c4f2763e12462541a0076f3c22
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-10 20:53:20 -0400

    Task 0 hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,8 +10,9 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
